f916b93b3e27a94d2d72bbd9d87f5fad1992ab3c
Dattatray Panchal <dattapanchal464@gmail.com>
2025-09-25 11:03:52 +0530

    created user model , message model , db index

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileUrl: {
+      type: String,
+      default: "", 
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
